ef4345314d6741ff6b6554545ed30193cb76d463
咸鱼主义者Official <160820048+Frysland-2024@users.noreply.github.com>
2026-07-27 12:48:20 +0800

    Export ML4pXRDs ResNet backbone

--- xrd_robustness/src/xrd_robustness/models/__init__.py
+++ xrd_robustness/src/xrd_robustness/models/__init__.py
@@ -1,12 +1,15 @@
 """Project-owned V7 PXRD backbones."""
 
 from .derivative_channels import DerivativeChannels
+from .ml4pxrd_resnet1d import ML4PXRDResNet1D, ML4PXRDResNet1DConfig
 from .patch_transformer import BackboneOutput, PatchTransformerConfig, XRDPatchTransformer
 from .xrd_pampt import PAMPT, PAMPTConfig
 
 __all__ = [
     "BackboneOutput",
     "DerivativeChannels",
+    "ML4PXRDResNet1D",
+    "ML4PXRDResNet1DConfig",
     "PAMPT",
     "PAMPTConfig",
     "PatchTransformerConfig",
